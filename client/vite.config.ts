69c00bf195ed6c1638f62c8725c4573dd1b8ff48
heitzjulien <heitzjulien@pm.me>
2024-10-30 09:15:32 +0100

    Update Dockerfiles and ports, introduce server Dockerfile

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
+    port: 4124,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8080",
+    origin: "http://0.0.0.0:4124",
   },
 })
