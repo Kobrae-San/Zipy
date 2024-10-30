289b6b5d75328a05edde06f6800d0233b90af436
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-30 16:14:57 +0100

    refactor: remove unused GetCredentials route from authRouter

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,14 +1,9 @@
 import Express, { Request, Response, NextFunction, Router } from "express";
-import {
-  Register,
-  Login,
-  GetCredentials,
-} from "../controllers/auth.controller";
+import { Register, Login } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post("/register", Register);
-authRouter.get("/register", GetCredentials);
 
 authRouter.post("/login", Login);
 
