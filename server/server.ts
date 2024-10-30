fa9a15afaebbefcbf298a2bb00f221e5eafee8e0
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-30 15:43:46 +0100

    refactor: update database connection to use client instead of pool

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,27 @@
 import Express, { Request, Response, NextFunction } from "express";
 import router from "./src/routes/router";
 import { envConfig } from "./src/configs/env.config";
-import { connectionDatabase, pool } from "./src/configs/database.config";
+import { connectionDatabase, client } from "./src/configs/database.config";
 import cors from "cors";
 
 envConfig();
-await connectionDatabase(pool);
+await connectionDatabase(client);
 
-const PORT: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3000;
+const PORT: number = process.env.SERVER_PORT
+  ? parseInt(process.env.SERVER_PORT)
+  : 3000;
 const app = Express();
 
-app.use(cors({
-    origin: 'http://localhost:4124',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:4124",
+    credentials: true,
+  })
+);
 
 app.use(Express.json());
 app.use("/api", router);
 
-app.listen(PORT, "0.0.0.0", () => { console.log(`Server is running on port ${PORT}`)});
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
