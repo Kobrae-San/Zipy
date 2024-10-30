f107fae8c0eb3f4de702e94a5eb0eed0bc87d9d8
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-30 15:44:06 +0100

    feat: add GetCredentials route to auth router for retrieving user credentials

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,14 @@
 import Express, { Request, Response, NextFunction, Router } from "express";
-import { Register, Login } from "../controllers/auth.controller";
+import {
+  Register,
+  Login,
+  GetCredentials,
+} from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post("/register", Register);
+authRouter.get("/register", GetCredentials);
 
 authRouter.post("/login", Login);
 
