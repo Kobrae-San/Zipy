50f7dc4f81a6ea1b5fdd977460ad37292b8a5b16
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-30 13:14:07 +0100

    Initialize Express server and configure routes and database connection

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import Express, { Request, Response, NextFunction } from "express";
+import router from "./src/routes/router";
+import { envConfig } from "./src/configs/env.config";
+import { connectionDatabase, pool } from "./src/configs/database.config";
+
+envConfig();
+await connectionDatabase(pool);
+
+const PORT = process.env.SERVER_PORT;
+const app = Express();
+
+app.use(Express.json());
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
