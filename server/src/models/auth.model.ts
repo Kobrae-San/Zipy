00591dca60ed7cc3b94718c7e26fc24868e46bf2
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-30 16:14:51 +0100

    refactor: remove unused GetUserCredentials function and clean up RegisterModel

--- server/src/models/auth.model.ts
+++ server/src/models/auth.model.ts
@@ -1,7 +1,6 @@
 import { client } from "../configs/database.config";
 
 export async function RegisterModel(nickname: string, password: string) {
-  console.log("coucou");
   return await client.query(
     `
         INSERT INTO users (nickname, password)  VALUES ($1, $2);
@@ -14,15 +13,6 @@ export async function LoginModel(nickname: string) {
   return client.query(
     `SELECT * FROM users WHERE nickname = $1;
       `,
-    [nickname]
-  );
-}
-
-export async function GetUserCredentials(nickname: string) {
-  return await client.query(
-    `
-        SELECT nickname, password FROM users WHERE nickname = $1
-    `,
     [nickname]
   );
 }
