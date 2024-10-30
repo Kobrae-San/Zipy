4192d0e803ffc400a04fe44e83334a72daa3b8d2
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-30 16:44:34 +0100

    feat: implement JWT token generation in login response

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { LoginModel, RegisterModel } from "../models/auth.model";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../configs/jwt.config";
 import bcrypt from "bcrypt";
 
 export async function Register(
@@ -49,11 +51,19 @@ export async function Login(req: Request, res: Response, next: NextFunction) {
       });
     }
 
+    const token = jwt.sign(
+      { userId: user.id, nickname: user.nickname },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN }
+    );
+
     return res.status(200).json({
       success: true,
       message: "Login successful",
-      data: {
-        user: { id: user.id, nickname: user.nickname },
+      token,
+      user: {
+        id: user.id,
+        nickname: user.nickname,
       },
     });
   } catch (error) {
