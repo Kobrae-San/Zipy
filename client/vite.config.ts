10cb0a5cf87622a671fda250b4589d321677c017
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-31 18:48:53 +0100

    refactor: clean up Vite configuration and remove unused proxy settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,16 +11,9 @@ export default defineConfig({
     },
   },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1',
-        changeOrigin: true,
-        secure: false,
-      }
-    },
     port: 4124,
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:4124",
   },
-})
+});
