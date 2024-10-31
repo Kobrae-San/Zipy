288a9a4477afca77fe973065b7607c8c2f66c694
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-31 18:49:19 +0100

    feat: integrate multer for file upload handling in file.routes

--- server/src/routes/file.routes.ts
+++ server/src/routes/file.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { getFiles, uploadFilesToDb } from "../controllers/file.controller";
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const fileRouter = Router();
 
 fileRouter.get("/history", getFiles);
-fileRouter.post("/upload", uploadFilesToDb);
+fileRouter.post("/upload", upload.single("file"), uploadFilesToDb);
 
 export default fileRouter;
