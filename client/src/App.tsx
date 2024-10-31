768454e9e140c429817ad850a0ba1b34d72b2f07
Kobrae-San <djedjegboble.pro@gmail.com>
2024-10-31 18:49:09 +0100

    feat: implement authentication context in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
-import './App.css';
+import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "@/pages/router.tsx";
+import { useState } from "react";
+
+import { AuthContext, UserContext } from "./hooks/context";
 
 function App() {
-    return (
-        <>
-            <RouterProvider router={router} />
-        </>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [userId, setUserId] = useState<number | undefined>(undefined);
+  return (
+    <>
+      <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        <UserContext.Provider value={{ userId, setUserId }}>
+          <RouterProvider router={router} />
+        </UserContext.Provider>
+      </AuthContext.Provider>
+    </>
+  );
 }
 
 export default App;
