77b30d6a0330d9af6c6475e4640fb0814d30434f
heitzjulien <heitzjulien@pm.me>
2024-10-31 09:39:11 +0100

    Update token verification to use cookies

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,25 +1,23 @@
-// middleware/auth.ts
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../configs/jwt.config";
+import jwt from "jsonwebtoken";
 
 export const verifyToken = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).json({ message: "No token provided" });
+    return res.status(401).json({ message: 'Authentication required' });
   }
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET!);
-    req.user = decoded;
-    console.log(req);
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.body = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: 'Invalid token' });
   }
 };
