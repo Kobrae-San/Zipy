54576ecc83dc51f536d1d7ccd269f330c94a9a00
Kobrae-San <djedjegboble.pro@gmail.com>
2024-11-01 22:24:03 +0100

    feat: add generateShareLink route for sharing files

--- server/src/routes/file.routes.ts
+++ server/src/routes/file.routes.ts
@@ -4,6 +4,7 @@ import {
   uploadFilesToDb,
   downloadFile,
   deleteFile,
+  generateShareLink,
 } from "../controllers/file.controller";
 import multer from "multer";
 
@@ -14,6 +15,7 @@ const fileRouter = Router();
 
 fileRouter.get("/history/:userId", getFiles);
 fileRouter.post("/upload", upload.single("file"), uploadFilesToDb);
+fileRouter.get("/share/:fileId(\\d+)", generateShareLink);
 fileRouter.get("/download/:fileId(\\d+)", downloadFile);
 fileRouter.delete("/file/:fileId(\\d+)", deleteFile);
 
