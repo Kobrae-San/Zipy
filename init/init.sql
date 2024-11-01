2ad7fcc525a85c00aeef7458dcca7e2b869b7cc7
Kobrae-San <djedjegboble.pro@gmail.com>
2024-11-01 22:23:02 +0100

    feat: add foreign key constraints to files table and create share_tokens table for file sharing

--- init/init.sql
+++ init/init.sql
@@ -13,5 +13,14 @@ CREATE TABLE files (
    file_data BYTEA NOT NULL,
    file_size BIGINT NOT NULL,
    mime_type VARCHAR(100),
-   uploaded_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+   uploaded_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY (id_user) REFERENCES users(id)
+);
+
+CREATE TABLE share_tokens (
+  id SERIAL PRIMARY KEY,
+  file_id INTEGER NOT NULL,
+  token VARCHAR(255) NOT NULL,
+  expires_at TIMESTAMP NOT NULL,
+  FOREIGN KEY (file_id) REFERENCES files(id)
 );
