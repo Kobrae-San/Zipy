0a0822fd246e5d27a0296d4c9c57f311e75ae924
Kobrae-San <djedjegboble.pro@gmail.com>
2024-11-01 19:04:45 +0100

    feat: add download and delete file routes; update history route to include userId

--- server/src/routes/file.routes.ts
+++ server/src/routes/file.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { getFiles, uploadFilesToDb } from "../controllers/file.controller";
+import {
+  getFiles,
+  uploadFilesToDb,
+  downloadFile,
+  deleteFile,
+} from "../controllers/file.controller";
 import multer from "multer";
 
 const storage = multer.memoryStorage();
@@ -7,7 +12,9 @@ const upload = multer({ storage: storage });
 
 const fileRouter = Router();
 
-fileRouter.get("/history", getFiles);
+fileRouter.get("/history/:userId", getFiles);
 fileRouter.post("/upload", upload.single("file"), uploadFilesToDb);
+fileRouter.get("/download/:fileId(\\d+)", downloadFile);
+fileRouter.delete("/file/:fileId(\\d+)", deleteFile);
 
 export default fileRouter;
--- server/src/routes/private.routes.ts
+++ server/src/routes/private.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import historyRouter from "./file.routes";
+
 import fileRouter from "./file.routes";
 
 const privateRouter = Router();
 
-privateRouter.use(historyRouter);
 privateRouter.use(fileRouter);
 
 export default privateRouter;
