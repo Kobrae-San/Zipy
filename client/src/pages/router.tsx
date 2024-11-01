7ac1174ee5a4136d5809ff1098b629eed37287ef
Kobrae-San <djedjegboble.pro@gmail.com>
2024-11-01 22:23:44 +0100

    feat: add download route for FileDownLoad component in router

--- client/src/pages/router.tsx
+++ client/src/pages/router.tsx
@@ -3,43 +3,47 @@ import Login from "@/pages/Auth/Login";
 import { Register } from "@/pages/Auth/Register.tsx";
 import History from "@/pages/History.tsx";
 import Home from "@/pages/Home.tsx";
+import FileDownLoad from "@/pages/FileDownLoad.tsx";
 import Nav from "@/components/Nav.tsx";
 import { Toaster } from "@/components/ui/toaster.tsx";
 
 const Layout = () => {
-    return (
-        <>
-            <Nav />
-            <Outlet />
-            <Toaster />
-        </>
-    );
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <Toaster />
+    </>
+  );
 };
 
-
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
         path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/history",
-                element: <History />,
-            },
-            {
-                path: "/login",
-                element: <Login />,
-            },
-            {
-                path: "/register",
-                element: <Register />,
-            },
-        ],
-    },
+        element: <Home />,
+      },
+      {
+        path: "/history",
+        element: <History />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/download/:token",
+        element: <FileDownLoad />,
+      },
+    ],
+  },
 ]);
 
 export default router;
