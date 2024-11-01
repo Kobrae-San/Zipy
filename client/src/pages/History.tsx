a91b4b51ef98a65f3a7b09ee3d26b60eba294bd2
Kobrae-San <djedjegboble.pro@gmail.com>
2024-11-01 19:04:14 +0100

    feat: integrate user context into History component for personalized data fetching

--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -1,41 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "@/hooks/context";
 import { toast } from "@/hooks/use-toast.ts";
 import HistoryList from "@/components/HistoryList.tsx";
 
 export default function History() {
-    const [data, setData] = useState(null);
-    const navigate = useNavigate();
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
+  const { userId } = useContext(UserContext);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("http://127.0.0.1:3000/api/private/history", {
-                    method: "GET",
-                    credentials: 'include'
-                });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://127.0.0.1:3000/api/private/history/${userId}`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
 
-                if (response.status === 401) {
-                    toast({
-                        variant: "destructive",
-                        description: "Invalid token",
-                    })
-                    navigate('/login');
-                }
+        if (response.status === 401) {
+          toast({
+            variant: "destructive",
+            description: "Invalid token",
+          });
+          navigate("/login");
+        }
 
-                const data = await response.json();
-                setData(data);
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
 
-        fetchData();
-    }, [navigate]);
+    fetchData();
+  }, [navigate]);
 
-    return (
-        <div className="container mx-auto p-8 max-w-4xl text-center">
-            <HistoryList/>
-        </div>
-    )
+  return (
+    <div className="container mx-auto p-8 max-w-4xl text-center">
+      <HistoryList />
+    </div>
+  );
 }
